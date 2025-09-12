63c033bb924b1a50409e9679327cda868733686d
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-09-12 11:11:24 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#161127]`}>
         {children}
         <ToastContainer />
+        <Analytics />
       </body>
     </html>
   );
