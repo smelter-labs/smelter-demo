5d1754ae22fe821a63fbc1423a3b83edb08ca3f6
Piotr Ruszczak <piotr.ruszczak@swmansion.com>
2025-10-06 21:20:53 +0200

    fix

--- components/control-panel/control-panel.tsx
+++ components/control-panel/control-panel.tsx
@@ -90,7 +90,6 @@ export default function ControlPanel({
       mounted = false;
     };
   }, []);
-
   // Spinner logic: show spinner for up to 10s or until at least one stream appears,
   // but never show spinner if there was ever any input added
   useEffect(() => {
