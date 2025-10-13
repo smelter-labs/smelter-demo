8a94c848e42e790e2a5e4c9518cef6865c6a8224
Piotr Ruszczak <snow.piotrek@gmail.com>
2025-10-13 22:32:47 +0200

    refresh input

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -9,8 +9,8 @@ let BASE_URL = 'https://puffer.fishjam.io/smelter-demo-api';
 let WHIP_URL = 'https://puffer.fishjam.io/smelter-demo-whep';
 BASE_URL = 'https://puffer.fishjam.io/smelter-demo-api';
 WHIP_URL = 'https://puffer.fishjam.io/smelter-demo-whep';
-WHIP_URL = 'http://localhost:9000';
-BASE_URL = 'http://localhost:3001';
+//WHIP_URL = 'http://localhost:9000';
+//BASE_URL = 'http://localhost:3001';
 assert(BASE_URL);
 
 type ShaderParam = {
